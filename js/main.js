d0efdb61c6aa7b9433d3e15e78538337d4529abf
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-10-22 21:38:16 +0700

    Осталось только сделать точечное меню

--- js/main.js
+++ js/main.js
@@ -1,94 +1,210 @@
+// Блок SVG рисунка бургер-меню
 const headerBurgerMenu = document.querySelector('.header_burger__menu');
-// console.log('headerBurgerMenu: ', headerBurgerMenu.childNodes.svg);
 const hear = document.querySelector('.header_burger__menu svg path');
 
-const burger = document.querySelector('.burger');
-const burgerClose = document.querySelector('.burger_close');
-const burgerMenuList = document.querySelector('.burger_menu__list');
-
-// const docElements = document.documentElement.scrollHeight;
-const header = document.querySelector('.header');
-
+// Изменение цвета элементов header-а в зависимости от его цвета
 const burgerColor = document.querySelector('.burger_color');
 const logoColor = document.querySelector('.logo-color');
 const logoFillText = document.querySelector('.logo_fill__text');
 
-const sectionErrorButton = document.querySelector('.section-error_button');
+// Секция header-а
+const header = document.querySelector('.header');
+
+// Секция бургер-меню
+const burger = document.querySelector('.burger');
+const burgerClose = document.querySelector('.burger_close');
+const burgerMenuList = document.querySelector('.burger_menu__list');
+
+// Секция сообщения об ошибке
 const sectionError = document.querySelector('.section-error');
 
+// Секция сообщения об успешной отправке заявки
+const sectionRequestSuccess = document.querySelector(
+	'.section-request__success'
+);
+
+// Якорные ссылки на начало основных секций лендинга
+const sectionMain = document.querySelector('#section-main');
+const sectionAboutCompany = document.querySelector('#section-about-company');
+const sectionPortfolio = document.querySelector('#section-portfolio');
+const sectionPropositions = document.querySelector('#section-propositions');
+const specPropositions = document.querySelector('#spec-propositions');
+
+// Кнопка бургер-меню "Оставить заявку"
+const burgerWindowButton = document.querySelector('.burger_window__button');
+
+// Кнопка закрытия окна сообщения об ошибке
+const sectionErrorButton = document.querySelector('.section-error_button');
+
+// Кнопка перехода к портфолио
 const sectionPortfolioButton = document.querySelector(
 	'.section-portfolio_button'
 );
 
+// Кнопка подтверждения отправки
 const specSubmitButton = document.querySelector('.spec-submit_button');
 
-const sectionRequestSuccess = document.querySelector(
-	'.section-request__success'
-);
+// Кнопка успешной отправки заявки
 const successButton = document.querySelector('.success_button');
+const messageButtonBlock = document.querySelector('.message-button_block');
 
-specSubmitButton.addEventListener('click', () => {
-	sectionRequestSuccess.classList.remove('hidden');
-	document.body.style.overflow = 'hidden';
+// Пункты бургер-меню
+const burgerMenuItems = document.querySelectorAll('.burger_menu__item');
+
+// Секция точечного меню
+const sectionScrollMenu = document.querySelector('.section_scroll-menu');
+
+// Кнопка "Спец предложение" первого экрана
+const sectionMainButton = document.querySelector('.section-main_button');
+
+// Логотип header-а
+const headerLogoIcon = document.querySelector('.header_logo__icon');
+
+// Доработать функцию обработки точечного меню
+sectionScrollMenu.addEventListener('click', (event) => {
+	event.target.classList.add('section_scroll-item__active');
 });
 
-successButton.addEventListener('click', () => {
-	sectionRequestSuccess.classList.add('hidden');
-	document.body.style.overflow = '';
+// Обработка нажатия кнопки логотипа header-а
+headerLogoIcon.addEventListener('click', (event) => {
+	event.preventDefault();
+	const target = event.currentTarget;
+	const id = target.getAttribute('href').substring(1);
+	document.getElementById(id).scrollIntoView({
+		behavior: 'smooth',
+	});
 });
 
-sectionPortfolioButton.addEventListener('click', (e) => {
+// Обработка нажатия кнопки главного экрана "Спец предложение"
+sectionMainButton.addEventListener('click', (event) => {
+	event.preventDefault();
+	const target = event.currentTarget;
+	const id = target.getAttribute('href').substring(1);
+	document.getElementById(id).scrollIntoView({
+		behavior: 'smooth',
+	});
+});
+
+// Обработка кнопки "Перейти на сайт" и открытие сообщения об ошибке
+sectionPortfolioButton.addEventListener('click', (event) => {
+	event.preventDefault();
 	// Исключаем прокрутку документа под открытым окном сообщения об ошибке
 	document.body.style.overflow = 'hidden';
-	sectionError.classList.remove('hidden');
+	// sectionError.classList.remove('hidden');
+	sectionError.classList.remove('fade_left');
+	sectionError.classList.add('appear_left');
 });
 
-// Закрытие окна сообщения об ошибке и переход в начало страницы через якорную ссылку
-sectionErrorButton.addEventListener('click', () => {
-	sectionError.classList.add('hidden');
+// Закрытие окна сообщения об ошибке и переход в начало страницы
+sectionErrorButton.addEventListener('click', (event) => {
+	event.preventDefault();
+	// sectionError.classList.add('hidden');
 	document.body.style.overflow = '';
+	const target = event.currentTarget;
+	const id = target.getAttribute('href').substring(1);
+	document.getElementById(id).scrollIntoView({});
+	sectionError.classList.remove('appear_left');
+	sectionError.classList.add('fade_left');
 });
 
-// Изменение фона header-а при скроллинге страницы
-document.addEventListener('scroll', () => {
-	if (window.pageYOffset !== 0) {
-		headerBackgroundDark();
-	} else {
-		headerBackgroundTransparent();
-	}
+// Обработка нажатия кнопки отправки заявки и отображение окна успешной отправки
+specSubmitButton.addEventListener('click', (event) => {
+	event.preventDefault();
+	document.body.style.overflow = 'hidden';
+	// sectionRequestSuccess.classList.remove('hidden');
+	sectionRequestSuccess.classList.remove('fade_left');
+	sectionRequestSuccess.classList.add('appear_left');
+});
+
+// Обработка закрытия окна успешной отправки
+messageButtonBlock.addEventListener('click', (event) => {
+	event.preventDefault();
+	// sectionRequestSuccess.classList.add('hidden');
+	document.body.style.overflow = '';
+	const id = event.currentTarget.getAttribute('data-link').substring(1);
+	document.getElementById(id).scrollIntoView({});
+	sectionRequestSuccess.classList.remove('appear_left');
+	sectionRequestSuccess.classList.add('fade_left');
+});
+
+// Обработка нажатия на пункт бургер-меню с плавным скроллингом к выбранной секции
+burgerMenuItems.forEach((burgerMenuItem) => {
+	burgerMenuItem.addEventListener('click', (event) => {
+		event.preventDefault();
+		closeBurger();
+		const target = event.target;
+		const id = target.getAttribute('href').substring(1);
+		document.getElementById(id).scrollIntoView({
+			behavior: 'smooth',
+		});
+	});
 });
 
 // Открываем бургер-меню
 headerBurgerMenu.addEventListener('click', () => {
-	burger.classList.toggle('hidden');
+	// burger.classList.remove('hidden');
+	burger.classList.add('appear');
+	burger.classList.remove('fade');
+
 	// Исключаем прокрутку документа под открытым модальным окном
 	document.body.style.overflow = 'hidden';
 });
 
 // Скрываем бургер-меню по нажатию на крестик
 burgerClose.addEventListener('click', () => {
-	burger.classList.toggle('hidden');
-	// Разрешаем прокрутку документа под открытым модальным окном
-	document.body.style.overflow = '';
+	closeBurger();
 });
 
-// Скрываем бургер-меню по нажатию на пункт меню
-burgerMenuList.addEventListener('click', () => {
-	burger.classList.toggle('hidden');
+// Скрываем бургер-меню по нажатию на кнопку "Отправить заявку"
+// и переходим по ссылке
+burgerWindowButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	closeBurger();
+	// Плавный скроллинг по ссылке #specPropositions
+	specPropositions.scrollIntoView({ behavior: 'smooth' });
+});
+
+// При начальной загрузке страницы делаем прозрачным header
+initialHeader();
+
+// Функция закрытия бургер-меню
+function closeBurger() {
+	// burger.classList.add('hidden');
+	burger.classList.add('fade');
+	burger.classList.remove('appear');
+
 	// Разрешаем прокрутку документа под открытым модальным окном
 	document.body.style.overflow = '';
+}
+
+// Изменение фона header-а при скроллинге страницы
+document.addEventListener('scroll', () => {
+	initialHeader();
 });
+
+// Управляет прозрачностью header-а
+function initialHeader() {
+	if (window.pageYOffset <= 400) {
+		headerBackgroundTransparent();
+	} else {
+		headerBackgroundDark();
+	}
+}
 
 // Функция изменения фона header-а на чёрный
+// и элементов header-а на белый
 function headerBackgroundDark() {
 	header.classList.remove('transparent');
+	header.classList.add('black');
 	burgerColor.style.fill = 'white';
 	logoColor.style.stroke = 'white';
 	logoFillText.style.fill = 'white';
 }
 
 // Функция изменения фона header-а на прозрачный
+// и элементов header-а на чёрный
 function headerBackgroundTransparent() {
+	header.classList.remove('black');
 	header.classList.add('transparent');
 	burgerColor.style.fill = 'black';
 	logoColor.style.stroke = 'black';
